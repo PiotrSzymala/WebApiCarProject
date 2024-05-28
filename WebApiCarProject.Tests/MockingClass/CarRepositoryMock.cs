5b44b929f0290ede82ede5e2abb380f2b1b019db
Piotr Szymala <p.szymala@sellpander.pl>
2024-05-28 19:03:24 +0200

    Extract mocking classes to separate filles.

---
+++ WebApiCarProject.Tests/MockingClass/CarRepositoryMock.cs
@@ -0,0 +1,47 @@
+using WebApiCarProject.Infrastructure.Entities;
+using WebApiCarProject.Infrastructure.Repositories;
+
+namespace WebApiCarProject.Tests.MockingClass;
+
+public class CarRepositoryMock : ICarRepository
+{
+    private readonly List<Car> _cars = new();
+
+    public Task<Car> GetCarAsync(long id)
+    {
+        if (id != 1)
+            return Task.FromResult<Car>(null);
+
+        return Task.FromResult(new Car { Id = 1, Brand = "Toyota", Model = "Corolla", Year = 2020 });
+    }
+
+    public async Task<IEnumerable<Car>> GetAllCarsAsync()
+    {
+        return new List<Car>
+        {
+            new Car { Id = 1, Brand = "Toyota", Model = "Corolla", Year = 2020 },
+            new Car { Id = 2, Brand = "Honda", Model = "Civic", Year = 2019 }
+        };
+    }
+
+    public Task InsertCarAsync(Car car)
+    {
+        _cars.Add(car);
+        return Task.CompletedTask;
+    }
+
+    public Task DeleteCarAsync(long id)
+    {
+        var car = _cars.Find(c => c.Id == id);
+        if (car != null)
+        {
+            _cars.Remove(car);
+        }
+        return Task.CompletedTask;
+    }
+
+    public Task SaveAsync()
+    {
+        return Task.CompletedTask;
+    }
+}
---
+++ WebApiCarProject.Tests/MockingClass/MediatorMock.cs
@@ -0,0 +1,42 @@
+﻿using MediatR;
+
+namespace WebApiCarProject.Tests.MockingClass;
+
+public class MediatorMock : IMediator
+{
+    public bool CommandResult { get; set; }
+
+    public Task<TResponse> Send<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default)
+    {
+        return Task.FromResult((TResponse)(object)CommandResult);
+    }
+
+    public Task<object> Send(object request, CancellationToken cancellationToken = default)
+    {
+        return Task.FromResult((object)CommandResult);
+    }
+
+    public Task Publish(object notification, CancellationToken cancellationToken = default)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task Publish<TNotification>(TNotification notification, CancellationToken cancellationToken = default) where TNotification : INotification
+    {
+        return Task.CompletedTask;
+    }
+    public IAsyncEnumerable<TResponse> CreateStream<TResponse>(IStreamRequest<TResponse> request,
+        CancellationToken cancellationToken = new CancellationToken())
+    {
+        throw new NotImplementedException();
+    }
+    public Task Send<TRequest>(TRequest request, CancellationToken cancellationToken = new CancellationToken()) where TRequest : IRequest
+    {
+        throw new NotImplementedException();
+    }
+
+    public IAsyncEnumerable<object?> CreateStream(object request, CancellationToken cancellationToken = new CancellationToken())
+    {
+        throw new NotImplementedException();
+    }
+}
