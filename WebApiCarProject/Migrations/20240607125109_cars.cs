142d6f9e99d6a056994bcef601c11ba21a4bf1e7
Piotr Szymala <p.szymala@sellpander.pl>
2024-06-07 15:03:55 +0200

    Add cars table.

---
+++ WebApiCarProject/Migrations/20240607125109_cars.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApiCar.Migrations
+{
+    /// <inheritdoc />
+    public partial class cars : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Model",
+                table: "Cars",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Year",
+                table: "Cars",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Model",
+                table: "Cars");
+
+            migrationBuilder.DropColumn(
+                name: "Year",
+                table: "Cars");
+        }
+    }
+}
