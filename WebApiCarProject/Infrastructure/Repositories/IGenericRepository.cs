e7cc579e47b404d594082609fb711387c13fa5ac
Piotr Szymala <p.szymala@sellpander.pl>
2024-06-10 17:34:04 +0200

    Create generic repository.

---
+++ WebApiCarProject/Infrastructure/Repositories/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq.Expressions;
+
+namespace WebApiCarProject.Infrastructure.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        Task<List<T>> GetAllAsync();
+        T GetById(int id);
+        Task<T> GetByIdAsync(int id);
+        void Remove(T sender);
+        void Add(in T sender);
+        void Update(in T sender);
+        int Save();
+        Task<int> SaveAsync();
+        public T Select(Expression<Func<T, bool>> predicate);
+        public Task<T> SelectAsync(Expression<Func<T, bool>> predicate);
+    }
+}
